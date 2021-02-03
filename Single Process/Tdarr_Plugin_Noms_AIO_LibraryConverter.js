891f55927b04b4cb9b4ea5b70b339eea5c9e87c5
Nomsplease <Nomsplease.code@gmail.com>
2021-02-02 20:54:03 -0500

    Now subtitle section is done, start audio.. Move input validation to function

--- Single Process/Tdarr_Plugin_Noms_AIO_LibraryConverter.js
+++ Single Process/Tdarr_Plugin_Noms_AIO_LibraryConverter.js
@@ -44,13 +44,6 @@ function details() {
                   true,false`,
       },
       {
-        name: 'audio_undtermined_track_tag',
-        tooltip: `Specify language tag here for any unknown tracks. We will only use this if we only have one audio stream and need to tag it for further processing.
-                  \\nMust follow ISO-639-2 3 letter format. https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
-                  \\nExample:\\n
-                  eng`,
-      },
-      {
         name: 'container',
         tooltip: `Specify output container of file
                   \\n Ensure that all stream types you may have are supported by your chosen container.
@@ -60,6 +53,216 @@ function details() {
       },
     ],
   };
+}
+
+/**
+ * Handles logging in a standardised way.
+ */
+class Log {
+  constructor() {
+    this.entries = [];
+  }
+
+  /**
+   *
+   * @param {String} entry the log entry string
+   */
+  Add(entry) {
+    this.entries.push(entry);
+  }
+
+  /**
+   *
+   * @param {String} entry the log entry string
+   */
+  AddSuccess(entry) {
+    this.entries.push(`☑ ${entry}`);
+  }
+
+  /**
+   *
+   * @param {String} entry the log entry string
+   */
+  AddWarning(entry) {
+    this.entries.push(`⚠ ${entry}`)
+  }
+
+  /**
+   *
+   * @param {String} entry the log entry string
+   */
+  AddError(entry) {
+    this.entries.push(`☒ ${entry}`);
+  }
+
+  /**
+   * Returns the log lines separated by new line delimiter.
+   */
+  GetLogData() {
+    return this.entries.join("\n");
+  }
+}
+
+/**
+ * Handles the storage of FFmpeg configuration.
+ */
+class Configurator {
+  constructor(defaultOutputSettings = null) {
+    this.shouldProcess = false;
+    this.outputSettings = defaultOutputSettings || [];
+    this.inputSettings = [];
+  }
+
+  AddInputSetting(configuration) {
+    this.inputSettings.push(configuration);
+  }
+
+  AddOutputSetting(configuration) {
+    this.shouldProcess = true;
+    this.outputSettings.push(configuration);
+  }
+
+  RemoveOutputSetting(configuration) {
+    var index = this.outputSettings.indexOf(configuration);
+
+    if (index === -1) return;
+    this.outputSettings.splice(index, 1);
+  }
+
+  GetOutputSettings() {
+    return this.outputSettings.join(" ");
+  }
+
+  GetInputSettings() {
+    return this.inputSettings.join(" ");
+  }
+}
+
+function inputValidation(inputs,logger) {
+  try {
+    if (inputs.subtitle_language === '') {
+      logger.AddError(`subtitle_language options not set, please configure all options.`);
+      return false;
+    }
+    if (inputs.subtitle_keep_commentary === '') {
+      logger.AddError(`subtitle_keep_commentary options not set, please configure all options.`);
+      return false;
+    }
+    if (inputs.subtitle_remove_unknown === '') {
+      logger.AddError(`subtitle_remove_unknown options not set, please configure all options.`);
+      return false;
+    }
+    if (inputs.audio_language === '') {
+      logger.AddError(`audio_language options not set, please configure all options.`);
+      return false;
+    }
+    if (inputs.audio_keep_commentary === '') {
+      logger.AddError(`audio_keep_commentary options not set, please configure all options.`);
+      return false;
+    }
+    if (inputs.container === '') {
+      logger.AddError(`'container options not set, please configure all options.`);
+      return false;
+    }
+  } catch(err) {
+    logger.AddError(`Settings not configured. Every setting is required.`);
+  }
+}
+
+function streamByTypeToArray(type,array,file,logger) {
+  for (let i = 0; i < file.ffProbeData.streams.length; i++) {
+    try {
+      if (file.ffProbeData.streams[i].codec_type.toLowerCase() === type) {
+        array.push(i);
+      }
+    } catch (err) {
+      logger.AddError(`Hit error looking for ${type} in ${i} for array ${array}`)
+    }
+  }
+}
+
+function removeFromArray(array, stream) {
+	const index = array.indexOf(array[stream]);
+	array.splice(index, 1);
+}
+
+function subtitleUnknownFilter(inputs,array,file,logger,configuration) {
+  if (inputs.subtitle_remove_unknown === "true") {
+		for (stream in array) {
+			try {
+				if (typeof file.ffProbeData.streams[array[stream]].tags.language === "undefined"){
+					logger.AddWarning(`Removing unknown subtitle at stream: ${array[stream]}`);
+					configuration.AddOutputSetting(`-map -0:s:${array[stream]}`);
+					removeFromArray(array, stream);
+				} else if (file.ffProbeData.streams[array[stream]].tags.language.toLowerCase().includes('und')){
+					logger.AddWarning(`Removing unknown subtitle at stream: ${array[stream]}`);
+					configuration.AddOutputSetting(`-map -0:s:${array[stream]}`);
+					removeFromArray(array, stream);
+				} else {
+					//If we hit here this subtitle has language or tag, and gets to move onto the next round
+				}
+			} catch (err) {
+				logger.AddError(`Subtitle unknown loop hit an error on stream ${array[stream]}. Error: ${err}`)
+			}
+		}
+  } else {
+    logger.AddSuccess(`Removing of unkown subtitles has been disabled.`)
+  }
+}
+
+function subtitleCommentaryFilter(inputs,array,file,logger,configuration) {
+  if (inputs.subtitle_keep_commentary === "false") {
+    for (stream in array) {
+      try {
+        if (file.ffProbeData.streams[array[stream]].tags.title.toLowerCase().includes('commentary')){
+          logger.AddWarning(`Removing undesired commentary subtitle at stream: ${array[stream]}`);
+					configuration.AddOutputSetting(`-map -0:s:${array[stream]}`);
+					removeFromArray(array, stream);
+        } else if (file.ffProbeData.streams[array[stream]].tags.title.toLowerCase().includes('description')){
+          logger.AddWarning(`Removing undesired description subtitle at stream: ${array[stream]}`);
+					configuration.AddOutputSetting(`-map -0:s:${array[stream]}`);
+					removeFromArray(array, stream);
+        } else {
+          //If we hit here this subtitle is not unkown, and gets to move onto the next round
+
+        }
+      } catch (err) {
+        logger.AddError(`Subtitle commentary loop hit an error on stream ${array[stream]}. Error: ${err}`)
+      }
+    }
+  } else {
+    logger.AddSuccess(`Removing of commentary subtitles has been disabled.`)
+  }
+}
+
+function subtitleLanguageFilter(inputs,array,file,logger,configuration) {
+  const language = inputs.subtitle_language.split(',');
+  for (stream in array) {
+    try {
+      if (language.indexOf(file.ffProbeData.streams[array[stream]].tags.language.toLowerCase(),) === -1 ) {
+        logger.AddWarning(`Removing subtitle with language ${file.ffProbeData.streams[array[stream]].tags.language}`)
+				configuration.AddOutputSetting(`-map -0:s:${array[stream]}`);
+				removeFromArray(array, stream);
+      }
+    } catch (err) {
+      logger.AddError(`Subtitle Language loop hit an error on stream ${array[stream]}. Error: ${err}`);
+    }
+	}
+} 
+
+function runSubtitleFilters(inputs,array,file,logger) {
+  let configuration = new Configurator(["-c:s copy"]);
+  subtitleUnknownFilter(inputs,array,file,logger,configuration);
+	subtitleCommentaryFilter(inputs,array,file,logger,configuration);
+	subtitleLanguageFilter(inputs,array,file,logger,configuration);
+  return configuration;
+}
+
+function audioUknownFilter(inputs,array,file,logger,configuration){
+	inputs.aaudio_language
+	if(array.length <= 1){
+		
+	}
 }
 
 function plugin(file, librarySettings, inputs) {
@@ -74,6 +277,16 @@ function plugin(file, librarySettings, inputs) {
     infoLog: '',
   };
 
+  //Stream Indecies
+  let audioStreams = [];
+  let videoStreams = [];
+  let subtitleStreams = [];
+
+  //Global Variables
+  var logger = new Log();
+  let ffmpegCommandInsert = '';
+  let convert = false;
+
   // Check if file is a video. If it isn't then exit plugin.
   if (file.fileMedium !== 'video') {
     // eslint-disable-next-line no-console
@@ -84,211 +297,30 @@ function plugin(file, librarySettings, inputs) {
   }
 
   // Check for inputs
-  try {
-    if (inputs.subtitle_language === '') {
-      response.infoLog += '☒ subtitle_language options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-    if (inputs.subtitle_keep_commentary === '') {
-      response.infoLog += '☒ subtitle_keep_commentary options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-    if (inputs.subtitle_remove_unknown === '') {
-      response.infoLog += '☒ subtitle_remove_unknown options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-    if (inputs.audio_language === '') {
-      response.infoLog += '☒ audio_language options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-    if (inputs.audio_keep_commentary === '') {
-      response.infoLog += '☒ audio_keep_commentary options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-    if (inputs.audio_undtermined_track_tag === '') {
-      response.infoLog += '☒ audio_undtermined_track_tag options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-    if (inputs.container === '') {
-      response.infoLog += '☒ container options not set, please configure all options. \n';
-      response.processFile = false;
-      return response;
-    }
-  } catch(err) {
-    response.infoLog += `☒ Settings not configured. Every setting is required. \n`
-  }
+	if (inputValidation(inputs,logger) === false) {
+		response.infoLog += logger.GetLogData();
+		return response;
+	}
 
   //Subtitle Variables
-  const language = inputs.language.split(',');
-  let ffmpegCommandInsert = '';
-  let convert = false;
+  const language = inputs.subtitle_language.split(',');
   let audioIdx = 0;
-  let audioStreamsRemoved = 0;
-  const audioStreamCount = file.ffProbeData.streams.filter((row) => row.codec_type.toLowerCase() === 'audio',).length;
 
-  for (let i = 0; i < file.ffProbeData.streams.length; i++) {
-    try {
-      //If we only have one audio stream, taggin as we cannot remove it with the configured language.
-      if ( audioStreamCount <= 1 ) {
-        try {
-          if (
-            typeof file.ffProbeData.streams[i].tags.language === 'undefined'
-            || file.ffProbeData.streams[i].tags.language.toLowerCase().includes('und')
-            && file.ffProbeData.streams[i].codec_type.toLowerCase() === 'audio'
-          ) {
-            if (file.ffProbeData.streams[i].channels === 8) {
-              if (typeof file.ffProbeData.streams[i].tags.title === 'undefined' || file.ffProbeData.streams[i].tags.title === 'und') {
-                ffmpegCommandInsert += `-metadata:s:a:${audioIdx} title="7.1" `;
-                response.infoLog += `☒Audio stream detected as 8 channel with no title, tagging. Stream 0:a:${audioIdx} as ${inputs.und_tag_lang} \n`;
-                convert = true;
-                reQueueAfter = true;
-              } 
-              if (typeof file.ffProbeData.streams[i].tags.language === 'undefined' || file.ffProbeData.streams[i].tags.language !== "eng"){
-                ffmpegCommandInsert += `-metadata:s:a:${audioIdx} language=${inputs.und_tag_lang} `;
-                response.infoLog += `☒Audio stream detected as having no language, tagging as ${inputs.und_tag_lang}. \n`;
-                convert = true;
-                reQueueAfter = true;
-              }
-            }
-            if (file.ffProbeData.streams[i].channels === 6) {
-              if (typeof file.ffProbeData.streams[i].tags.title === 'undefined' || file.ffProbeData.streams[i].tags.title === 'und') {
-                ffmpegCommandInsert += `-metadata:s:a:${audioIdx} title="5.1"`;
-                response.infoLog += `☒Audio stream detected as 6 channel with no title, tagging. Stream 0:a:${audioIdx} as ${inputs.und_tag_lang} \n`;
-                convert = true;
-                reQueueAfter = true;
-              }
-              if (typeof file.ffProbeData.streams[i].tags.language === 'undefined' || file.ffProbeData.streams[i].tags.language !== "eng") {
-                ffmpegCommandInsert += `-metadata:s:a:${audioIdx} language=${inputs.und_tag_lang} `;
-                response.infoLog += `☒Audio stream detected as having no language, tagging as ${inputs.und_tag_lang}. \n`;
-                convert = true;
-                reQueueAfter = true;
-              }
-            }
-            if (file.ffProbeData.streams[i].channels === 2) {
-              if (typeof file.ffProbeData.streams[i].tags.title === 'undefined' || file.ffProbeData.streams[i].tags.title === 'und') {
-                ffmpegCommandInsert += `-metadata:s:a:${audioIdx} title="2.0" `;
-                response.infoLog += `☒Audio stream detected as 2 channel with no title, tagging. Stream 0:a:${audioIdx} as ${inputs.und_tag_lang} \n`;
-                convert = true;
-                reQueueAfter = true;
-              } 
-              if (typeof file.ffProbeData.streams[i].tags.language === 'undefined' || file.ffProbeData.streams[i].tags.language !== "eng") {
-                ffmpegCommandInsert += `-metadata:s:a:${audioIdx} language=${inputs.und_tag_lang} `;
-                response.infoLog += `☒Audio stream detected as having no language, tagging as ${inputs.und_tag_lang}. \n`;
-                convert = true;
-                reQueueAfter = true;
-              }
-            }
-          }
-        } catch (err) {
-          // Error
-        }
-      } else {
+  streamByTypeToArray("video", videoStreams, file)
+  streamByTypeToArray("audio", audioStreams, file)
+  streamByTypeToArray("subtitle", subtitleStreams, file)
 
-      let removeThisStream = false;
-      try {
-        if (
-          file.ffProbeData.streams[i].codec_type.toLowerCase() === 'audio'
-          && language.indexOf(
-            file.ffProbeData.streams[i].tags.language.toLowerCase(),
-          ) === -1
-          && removeThisStream === false
-        ) {
-          audioStreamsRemoved += 1;
-          removeThisStream = true;
-          ffmpegCommandInsert += `-map -0:a:${audioIdx} `;
-          response.infoLog += `☒Audio stream detected as being unwanted, removing. Audio stream 0:a:${audioIdx} \n`;
-          convert = true;
-        }
-      } catch (err) {
-        // Error
-      }
+  logger.AddSuccess(`Video streams are at: ${videoStreams}`);
+  logger.AddSuccess(`Audio streams are at: ${audioStreams}`);
+  logger.AddSuccess(`Subtitle streams are at: ${subtitleStreams}`);
+  let subtitleConfiguration = runSubtitleFilters(inputs,subtitleStreams,file,logger);
 
-      try {
-        if (
-          inputs.keep_commentary.toLowerCase() === 'false'
-          && file.ffProbeData.streams[i].codec_type.toLowerCase() === 'audio'
-          && (file.ffProbeData.streams[i].tags.title
-            .toLowerCase()
-            .includes('commentary')
-            || file.ffProbeData.streams[i].tags.title
-              .toLowerCase()
-              .includes('description')
-            || file.ffProbeData.streams[i].tags.title.toLowerCase().includes('sdh'))
-            && removeThisStream === false
-        ) {
-          audioStreamsRemoved += 1;
-          removeThisStream = true;
-          ffmpegCommandInsert += `-map -0:a:${audioIdx} `;
-          response.infoLog += `☒Audio stream detected as being commentary, removing. Stream 0:a:${audioIdx} \n`;
-          convert = true;
-        }
-      } catch (err) {
-        // Error
-      }
+  logger.AddWarning(`${subtitleConfiguration.GetInputSettings()},${subtitleConfiguration.GetOutputSettings()} -max_muxing_queue_size 4096`)
+  logger.AddWarning('End Plugin');
 
-      if (file.ffProbeData.streams[i].codec_type.toLowerCase() === 'audio') {
-        try {
-          if (file.ffProbeData.streams[i].tags.language.toLowerCase().includes('und') && removeThisStream === false) {
-            audioStreamsRemoved += 1;
-            removeThisStream = true;
-            ffmpegCommandInsert += `-map -0:a:${audioIdx} `;
-            response.infoLog += `☒Audio stream detected as having no language, removing. Audio stream 0:a:${audioIdx} \n`;
-            convert = true;
-          }
-        } catch (err) {
-          // Error
-        }
-
-        if (typeof file.ffProbeData.streams[i].tags === 'undefined' && removeThisStream === false) {
-          audioStreamsRemoved += 1;
-          removeThisStream = true;
-          ffmpegCommandInsert += `-map -0:a:${audioIdx} `;
-          response.infoLog += `☒Audio stream detected as having no language, removing. Audio stream 0:a:${audioIdx} \n`;
-          convert = true;
-        } else if (typeof file.ffProbeData.streams[i].tags.language === 'undefined' && removeThisStream === false) {
-          audioStreamsRemoved += 1;
-          removeThisStream = true;
-          ffmpegCommandInsert += `-map -0:a:${audioIdx} `;
-          response.infoLog += `☒Audio stream detected as having no language, removing. Audio stream 0:a:${audioIdx} \n`;
-          convert = true;
-        }
-      }
-
-      // Check if stream type is audio and increment audioIdx if true.
-      if (file.ffProbeData.streams[i].codec_type.toLowerCase() === 'audio') {
-        audioIdx += 1;
-      }
-      }
-    } catch(err) {
-
-    }
-  }
-
-  // Failsafe to cancel processing if all streams would be removed following this plugin. We don't want no audio.
-  if (audioStreamsRemoved === audioStreamCount) {
-    response.infoLog += `⚠ We have ${audioStreamCount} and we were attempting to remove ${audioStreamsRemoved}`
-    response.infoLog += '☒Cancelling plugin otherwise all audio tracks would be removed. \n';
-    response.processFile = false;
-    return response;
-  }
-
-  // Convert file if convert variable is set to true.
-  if (convert === true) {
-    response.processFile = true;
-    response.preset = `, -map 0 ${ffmpegCommandInsert} -c copy -max_muxing_queue_size 9999`;
-    response.container = `.${file.container}`;
-    response.reQueueAfter = true;
-  } else {
-    response.processFile = false;
-    response.infoLog += "☑File doesn't contain audio tracks which are unwanted.\n";
-  }
+  response.infoLog += logger.GetLogData();
   return response;
+  response.processFile = false;
 }
 module.exports.details = details;
 module.exports.plugin = plugin;
